725f105da0313c206f6037343e762497acc11230
Moch Deden <moch.deden.r@gmail.com>
2018-01-31 19:59:42 +0700

    Init ellipse top of rect

--- src/shape_game/core.clj
+++ src/shape_game/core.clj
@@ -2,28 +2,33 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def width 500)
-(def height 500)
+(def width 800)
+(def height 600)
+
 (def rect-width (/ width 4))
 (def rect-height (/ height 20))
-(def rect-y (- height rect-height 10))
 (def rect-x-init (/ (- width rect-width) 2))
-(def ellipse-x-init (+ rect-x-init (/ rect-width 2)))
+(def rect-y-init (- height rect-height 10))
+
 (def ellipse-wh (/ rect-width 10))
+(def ellipse-x-init (+ rect-x-init (/ rect-width 2)))
+(def ellipse-y-init (- rect-y-init (/ ellipse-wh 2)) )
 
 (defn setup []
   (q/frame-rate 60)
   (q/color-mode :hsb)
   {:rect-x rect-x-init
-   :ellipse-x ellipse-x-init})
+   :rect-y rect-y-init
+   :ellipse-x ellipse-x-init
+   :ellipse-y ellipse-y-init})
 
 (defn update-state [state]
   state)
 
 (defn draw-state [state]
   (q/background 240)
-  (q/ellipse (:ellipse-x state) 60 ellipse-wh ellipse-wh)
-  (q/rect (:rect-x state) rect-y rect-width rect-height))
+  (q/ellipse (:ellipse-x state) (:ellipse-y state) ellipse-wh ellipse-wh)
+  (q/rect (:rect-x state) (:rect-y state) rect-width rect-height))
 
 (q/defsketch shape-game
   :title "Shape game"
@@ -35,6 +40,6 @@
   :middleware [m/fun-mode]
   :key-pressed (fn [{:keys [rect-x] :as state} { :keys [key key-code] }]
                  (case key
-                   (:right) {:rect-x (+ (:rect-x state) 4)}
-                   (:left) {:rect-x (- (:rect-x state) 4)}
+                   (:right) (update state :rect-x (partial + 4)) 
+                   (:left) (update state :rect-x (fn [x] (- x 4)))
                    state)))
