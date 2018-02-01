6425d372bbcccbcee085a040c890c210d242c236
Moch Deden <moch.deden.r@gmail.com>
2018-02-01 10:05:14 +0700

    Ellipse x-direction follow rect when hit

--- src/shape_game/core.clj
+++ src/shape_game/core.clj
@@ -11,31 +11,48 @@
 (def rect-height (/ height 20))
 (def rect-x-init (/ (- width rect-width) 2))
 (def rect-y-init (- height rect-height 10))
-(def rect-speed 4)
+(def rect-x-step 4)
+(def rect-x-speed-init 0)
 (def rect-dir :none)
 
 (def ellipse-wh (/ rect-width 10))
 (def ellipse-x-init (+ rect-x-init (/ rect-width 2)))
 (def ellipse-y-init (- rect-y-init (/ ellipse-wh 2)) )
-(def ellipse-speed 4)
-(def ellipse-sign (atom -))
+(def ellipse-y-step 4)
+(def ellipse-x-speed-init 0)
+(def ellipse-sign-y (atom -))
 
 (defn setup []
   (q/frame-rate fps)
   (q/color-mode :hsb)
   {:rect-x rect-x-init
+   :rect-x-speed rect-x-speed-init
    :rect-y rect-y-init
    :rect-dir rect-dir
    :ellipse-x ellipse-x-init
+   :ellipse-x-speed ellipse-x-speed-init
+   :ellipse-sign-x +
+   :ellipse-sign-y -
    :ellipse-y ellipse-y-init})
 
 (defn update-state [state]
-  (cond
+  (->
+   (cond
     (= (:ellipse-y state) (/ ellipse-wh 2))
-    (swap! ellipse-sign (fn [_] +))
+    (do
+      (->
+       (update state :rect-x-speed (fn [_] 2))
+       (update :ellipse-sign-y (fn [_] +))
+       ))
     (= (:ellipse-y state) (+ (- rect-y-init rect-height) ellipse-wh))
-    (swap! ellipse-sign (fn [_] -)))
-  (update state :ellipse-y (fn [y] (@ellipse-sign y ellipse-speed))))
+    (do
+      (->
+       (update state :ellipse-x-speed (fn [_] (:rect-x-speed state)))
+       (update :ellipse-sign-y (fn [_] -))
+       (update :ellipse-sign-x (fn [y] (if (= :left (:rect-dir state)) + -)))))
+    :else
+    (update state :ellipse-x (fn [x] ((:ellipse-sign-x state) x (:ellipse-x-speed state)))))
+   (update :ellipse-y (fn [y] ((:ellipse-sign-y state) y ellipse-y-step)))))
 
 (defn draw-state [state]
   (q/background background-color)
@@ -51,7 +68,11 @@
   :features [:keep-on-top]
   :middleware [m/fun-mode]
   :key-pressed (fn [{:keys [rect-x] :as state} { :keys [key key-code] }]
-                 (case key
-                   (:right) (update state :rect-x (partial + rect-speed)) 
-                   (:left) (update state :rect-x (fn [x] (- x rect-speed)))
-                   state)))
+                 (->
+                  (case key
+                   (:right)
+                   (update state :rect-x (partial + rect-x-step))
+                   (:left)
+                   (update state :rect-x (fn [x] (- x rect-x-step)))
+                   state)
+                  (update :rect-dir (fn [_] key)))))
